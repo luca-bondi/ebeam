e3a95ff73c037a8974d9422fe9b2c05346258a0b
Luca Bondi <bondi.luca@gmail.com>
2021-04-22 18:56:54 -0400

    builds with migrated gui

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -10,9 +10,7 @@
  */
 class MainComponent  :
 public Component,
-private ValueTree::Listener,
-private Button::Listener,
-private ComboBox::Listener
+public EbeamerGUI::Callback
 {
 public:
     //==============================================================================
@@ -25,137 +23,30 @@ public:
     
 private:
     
-    String appVersion;
-    String appName;
-    const String appManufacturer = "Eventide";
+    //==============================================================================
+    /* GUI instance and utilities */
+    EbeamerGUI gui;
+    void showConnectionErrorMessage (const String&);
     
     //==============================================================================
-    const Identifier energyIdentifier = Identifier("doaEnergy");
-    Scene scene;
-    
-    //==============================================================================
-    Label steerLabel;
-    const Identifier steerX1Identifier = Identifier("steerBeamX1");
-    const Identifier steerX2Identifier = Identifier("steerBeamX2");
-    const Identifier steerY1Identifier = Identifier("steerBeamY1");
-    const Identifier steerY2Identifier = Identifier("steerBeamY2");
-    SteerSlider steerBeamX1Slider, steerBeamX2Slider;
-    SteerSlider steerBeamY1Slider, steerBeamY2Slider;
-    
-    //==============================================================================
-    Label widthLabel;
-    const Identifier width1Identifier = Identifier("widthBeam1");
-    const Identifier width2Identifier = Identifier("widthBeam2");
-    WidthSlider widthBeam1Knob, widthBeam2Knob;
-    
-    //==============================================================================
-    Label panLabel;
-    const Identifier pan1Identifier = Identifier("panBeam1");
-    const Identifier pan2Identifier = Identifier("panBeam2");
-    PanSlider panBeam1Knob, panBeam2Knob;
-    
-    //==============================================================================
-    Label levelLabel;
-    const Identifier level1Identifier = Identifier("levelBeam1");
-    const Identifier level2Identifier = Identifier("levelBeam2");
-    LevelSlider levelBeam1Knob, levelBeam2Knob;
-    
-    //==============================================================================
-    Label muteLabel;
-    const Identifier mute1Identifier = Identifier("muteBeam1");
-    const Identifier mute2Identifier = Identifier("muteBeam2");
-    MuteButton muteBeam1Button, muteBeam2Button;
-    
-    //==============================================================================
-    const Identifier inMetersIdentifier = Identifier("inMeters");
-    MultiChannelLedBar inputMeter;
-    const Identifier outMeter1Identifier = Identifier("outMeter1");
-    const Identifier outMeter2Identifier = Identifier("outMeter2");
-    SingleChannelLedBar beam1Meter, beam2Meter;
-    
-    //==============================================================================
-    const Identifier hpfIdentifier = Identifier("hpf");
-    HpfSlider hpfSlider;
-    
-    //==============================================================================
-    const Identifier gainIdentifier = Identifier("gainMic");
-    GainSlider gainSlider;
-    
-    //==============================================================================
-    /** CPU load component */
-    const Identifier cpuIdentifier = Identifier("cpuLoad");
-    CpuLoad cpuLoad;
-    
-    //==============================================================================
-    /** Swap side toggle component */
-    const Identifier frontIdentifier = Identifier("frontFacing");
-    FrontToggle frontToggle;
-    
-    //==============================================================================
-    /** Configuration selection combo */
-    const Identifier configIdentifier = Identifier("config");
-    ConfigComboBox configComboBox;
-    
-    
-    //==============================================================================
-    /* Layout functions */
-    void layoutConfigOsc(Rectangle<int>& );
-    
-    //==============================================================================
-    const std::vector<Colour> beamColours = {Colours::orangered, Colours::royalblue};
-    
-#ifdef TARGET_OS_IOS
-    const bool controlTextBoxEditable = false;
-#else
-    const bool controlTextBoxEditable = true;
-#endif
+    /* GUI callback */
+    bool oscConnect(const String& serverIp, int serverPort) override;
+    bool oscDisconnect(const String& serverIp, int serverPort) override;
+    bool isConnected() const override;
     
     //==============================================================================
     /** OSC */
     
     const float oscPollingFreq = 10;
-    
     bool connected;
+    
     OSCController oscController;
-    
-    const Identifier serversIdentifier = Identifier("servers");
-    std::map<int,Server> servers;
-    
-    Value serverIp;
-    Value serverPort;
-        
-    ComboBox oscIp;
-    Label oscIpLabel;
-    
-    TextEditor oscPort;
-    Label oscPortLabel;
-    
-    TextButton oscConnectButton;
-    
-    void oscConnect();
-    void oscDisconnect();
-    
-    //==============================================================================
-    /* Listeners and callbacks */
-    
-    void buttonClicked (Button*) override;
-    void comboBoxChanged(ComboBox *) override;
-    void showConnectionErrorMessage (const String&);
     
     //==============================================================================
     /* ValueTree */
     File statusFile;
-    ValueTree valueTreePersistent;
-    ValueTree valueTreeSession;
+    ValueTree valueTree;
     ValueTreeFile valueTreeFile;
-    
-    Identifier serverIpIdentifier = Identifier("serverIp");
-    Identifier serverPortIdentifier = Identifier("serverPort");
-    
-    
-    void valueTreePropertyChanged (ValueTree& treeWhosePropertyHasChanged,
-                                           const Identifier& property) override;
-    
     
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
--- Source/ebeamerDefs.h
+++ Source/ebeamerDefs.h
@@ -16,36 +16,4 @@
 #define MAX_HEIGHT 1366
 #define MAX_WIDTH 1366
 
-#define SMALL_MARGIN 5
-#define MEDIUM_MARGIN 10
-#define LARGE_MARGIN 15
-
-/** High of credits text plus a bit */
-#define BOTTOM_MARGIN 16
-
-#define HOR_SLIDER_HEIGHT 25
-#define VER_SLIDER_WIDTH 70
-
-#define LABEL_WIDTH 70
-#define LABEL_HEIGHT 20
-
-#define CENTRAL_LABEL_WIDTH 80
-#define SMALL_LABEL_WIDTH 25
-
-#define KNOB_SIZE 80
-#define MUTE_SIZE 40
-
-#define LED_SIZE 5
-
-#define OSC_IP_LABEL_WIDTH 20
-#define OSC_IP_WIDTH 150
-
-#define OSC_PORT_LABEL_WIDTH 60
-#define OSC_PORT_WIDTH 55
-
-#define OSC_CONNECT_WIDTH 100
-#define OSC_CONNECT_MARGIN_LEFT 15
-
-#define CONTROLS_HEIGHT 30
-
 
